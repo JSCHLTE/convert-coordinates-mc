1252f60568f97e2ea1303b8926f6a7d3bdfe8592
phoaxy <54962416+phoaxy@users.noreply.github.com>
2021-01-12 19:47:27 -0500

    Added 'clear' button.

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -2,6 +2,7 @@ const xNum = document.querySelector("#xInput");
 const yNum = document.querySelector("#yInput");
 const zNum = document.querySelector("#zInput");
 const btnConvert = document.querySelector("button");
+const btnReset = document.querySelector(".btnReset")
 
 let xCoord = 0;
 let yCoord = 0;
@@ -10,15 +11,23 @@ let zCoord = 0;
 btnConvert.addEventListener("click", () => {
 
     xCoord = xNum.value / 8;
-    yCoord = yNum.value / 8;
+    yCoord = yNum.value / 1;
     zCoord = zNum.value / 8;
 
     xRound = Math.round(xCoord);
-    yRound = Math.round(yCoord);
+    yRound = yNum.value;
     zRound = Math.round(zCoord);
 
     netherCoords = (`${xRound}, ${yRound}, ${zRound}`);
 
     document.getElementById("coords-count").innerHTML = netherCoords;
     document.getElementById("coord-msg").innerHTML = "Go to these coords in the nether and place a portal.";
+});
+
+btnReset.addEventListener("click", () => {
+    document.getElementById("coords-count").innerHTML = "";
+    document.getElementById("coord-msg").innerHTML = "";
+    xNum.value = "";
+    yNum.value = 64;
+    zNum.value = "";
 });
